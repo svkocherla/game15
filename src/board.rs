074bee5a7e8d87d4b9e766948a4559f4bde0fdb9
sriharshak27 <sriharshakocherla27@gmail.com>
2024-01-23 16:24:35 -0500

    complete and test movement methods

--- src/board.rs
+++ src/board.rs
@@ -1,7 +1,7 @@
 // 4 by 4 board
 pub struct Board {
     grid: [[usize; 4]; 4],
-    // empty_location: (usize, usize),
+    empty_location: (usize, usize),
 }
 
 impl Board {
@@ -16,40 +16,26 @@ impl Board {
         }
 
         grid[3][3] = 0;
-        // let mut empty_location = (3,3);
+        let empty_location = (3,3);
 
-        Board { grid }
+        Board { grid, empty_location }
     }
 
-    pub fn display(&self) {
+    pub fn display(&self) -> String {
+        let mut curr = String::new();
         for row in &self.grid {
             for &num in row {
                 if num == 0 {
-                    print!("  #");
+                    curr.push_str("  #");
                 } else {
-                    print!("{:3}", num);
+                    curr.push_str(&format!("{:3}", num));
                 }
             }
-            println!()
+            curr.push('\n');
         }
+
+        curr
     }
-
-    // moves perform a move on the grid if possible, else do nothing
-    // pub fn move_left(&mut self) {
-
-    // }
-
-    // pub fn move_right(&mut self) {
-        
-    // }
-
-    // pub fn move_up(&mut self) {
-        
-    // }
-
-    // pub fn move_down(&mut self) {
-        
-    // }
 
     // pub fn shuffle(&mut self, moves: usize) {
 
@@ -58,4 +44,65 @@ impl Board {
     // pub fn is_complete(&self) -> bool {
     //     false
     // }
+
+    // moves perform a move on the grid if possible, else do nothing
+    pub fn move_left(&mut self) {
+        let (row, col) = self.empty_location;
+
+        // cannot move left
+        if col == 0 {
+            return
+        }
+
+        let tmp = self.grid[row][col - 1];
+        self.grid[row][col - 1] = 0;
+        self.grid[row][col] = tmp;
+
+        self.empty_location = (row, col - 1);
+    }
+
+    pub fn move_right(&mut self) {
+        let (row, col) = self.empty_location;
+
+        // cannot move right
+        if col == 3 {
+            return
+        }
+
+        let tmp = self.grid[row][col + 1];
+        self.grid[row][col + 1] = 0;
+        self.grid[row][col] = tmp;
+
+        self.empty_location = (row, col + 1);
+    }
+
+    pub fn move_up(&mut self) {
+        let (row, col) = self.empty_location;
+
+        // cannot move up
+        if row == 0 {
+            return
+        }
+
+        let tmp = self.grid[row - 1][col];
+        self.grid[row - 1][col] = 0;
+        self.grid[row][col] = tmp;
+
+        self.empty_location = (row - 1, col);
+    }
+
+    pub fn move_down(&mut self) {
+        let (row, col) = self.empty_location;
+
+        // cannot move down
+        if row == 3 {
+            return
+        }
+
+        let tmp = self.grid[row + 1][col];
+        self.grid[row + 1][col] = 0;
+        self.grid[row][col] = tmp;
+
+        self.empty_location = (row + 1, col);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,48 @@ use board::Board;
 
 fn main() {
     println!("Program Start");
-    let board = Board::new();
+
+    let mut board = Board::new();
     println!("Displaying initial board");
-    board.display();
+    println!("{}", board.display());
+
+    println!("Moving Left");
+    board.move_left();
+    println!("{}", board.display());
+
+    println!("Moving Left");
+    board.move_left();
+    println!("{}", board.display());
+
+    println!("Moving Left");
+    board.move_left();
+    println!("{}", board.display());
+
+    println!("Moving Left");
+    board.move_left();
+    println!("{}", board.display());
+
+
+    println!("Moving Right");
+    board.move_right();
+    println!("{}", board.display());
+
+    println!("Moving Right");
+    board.move_right();
+    println!("{}", board.display());
+
+
+    println!("Moving Up");
+    board.move_up();
+    println!("{}", board.display());
+
+
+    println!("Moving Down");
+    board.move_down();
+    println!("{}", board.display());
+
+    println!("Moving Down");
+    board.move_down();
+    println!("{}", board.display());
+
 }
